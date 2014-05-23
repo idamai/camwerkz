09be513b35e0a4d21b36148043f568d98da8d8a7
Shazwi <supershazwi@gmail.com>
2014-05-23 04:15:51 -0400

    update index.html

--- js/gmap.js
+++ js/gmap.js
@@ -8,12 +8,12 @@ $(document).ready(function(){
 function initMapsView() {
 	var mapOptions = {
 		center : new google.maps.LatLng(1.304395,103.797647),
-		draggable : false,
-		zoomControl : false,
-		scrollwheel : false,
+		draggable : true,
+		zoomControl : true,
+		scrollwheel : true	,
 		disableDoubleClickZoom : true,
 		zoom : 16,
-		disableDefaultUI : true,
+		disableDefaultUI : false,
 		mapTypeId : google.maps.MapTypeId.ROADMAP
 	};
 	gMap = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
